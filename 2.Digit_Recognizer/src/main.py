0a4810eaa73ffbb72d8a653efe8124c7bdda2f8f
wangyubo <1061322005@qq.com>
2019-03-04 10:03:09 +0800

    暂存

--- 2.Digit_Recognizer/src/main.py
+++ 2.Digit_Recognizer/src/main.py
@@ -7,15 +7,18 @@ import seaborn as sns
 train_df = pd.read_csv('../data/train.csv')
 
 Y_train_series = train_df['label']
-X_train_df = train_df.drop(labels='label', axis=1)
 
-del train_df
+sns.countplot(Y_train_series)
+# X_train_df = train_df.drop(labels='label', axis=1)
 
-X_train_df = X_train_df / 255.0
+# del train_df
 
-X_train_ndarray = X_train_df.values.reshape(-1, 28, 28, 1)
+# X_train_df = X_train_df / 255.0
 
-print(X_train_ndarray.shape)
+# X_train_ndarray = X_train_df.values.reshape(-1, 28, 28, 1)
 
-plt.imshow(X_train_ndarray[5][:, :, 0])
+# print(X_train_ndarray.shape)
+
+# plt.imshow(X_train_ndarray[5][:, :, 0])
+
 plt.show()
